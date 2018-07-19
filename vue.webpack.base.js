9eb83a5f29da6086b191127edde876d28e674d44
lihaoran <lihaoran@cmcm.com>
2018-07-19 13:10:08 +0800

    fix config

--- vue.webpack.base.js
+++ vue.webpack.base.js
@@ -1,11 +1,4 @@
-const nodeExternals = require('webpack-node-externals')
-
 module.exports = {
   entry: './src/entry-client.js',
-  target: 'web',
-  externals: [
-    nodeExternals({
-      whitelist: ['jquery']
-    })
-  ]
+  target: 'web'
 }
